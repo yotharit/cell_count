05fb242f612dc1efc929ecf6b794f8220efaa9a8
Tharit Pongsaneh <tharit@wongnai.com>
2020-03-31 21:13:20 +0700

    p

--- app/src/main/java/com/waevploy/cellcount/cell/CellContract.kt
+++ app/src/main/java/com/waevploy/cellcount/cell/CellContract.kt
@@ -9,16 +9,18 @@ class CellContract : BaseMvpContract {
 	interface View : BaseMvpContract.View {
 		fun alertOpenCv(cvAlert: Boolean)
 		fun setResult(text: String)
+		fun cropImage()
+		fun cropMinRadius(cropImageUri: Uri)
+		fun cropMaxRadius(cropImageUri: Uri)
+		fun setImage(bmp: Bitmap)
 	}
 
 	interface Presenter : BaseMvpContract.Presenter<View> {
-		fun findCellFromPlate(
-			uri: Uri,
+		fun handleLogic(uri: Uri)
+		fun setValue(
 			brightness: Double,
 			contrast: Double,
-			minDistant: Double,
-			minRadius: Int,
-			maxRadius: Int
-		): Bitmap?
+			minDistant: Double
+		)
 	}
 }
--- app/src/main/java/com/waevploy/cellcount/cell/CellFragment.kt
+++ app/src/main/java/com/waevploy/cellcount/cell/CellFragment.kt
@@ -2,6 +2,8 @@ package com.waevploy.cellcount.cell
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -39,13 +41,44 @@ class CellFragment : BaseMvpFragment<CellContract.View, CellContract.Presenter>(
 
 	}
 
+	override fun setImage(bmp: Bitmap) {
+		imageView.setImageBitmap(bmp)
+	}
+
+	override fun cropImage() {
+		context?.let {
+			CropImage.activity()
+				.setActivityTitle("Cropping Plate")
+				.setGuidelines(CropImageView.Guidelines.ON)
+				.start(it, this)
+		}
+	}
+
+	override fun cropMinRadius(cropImageUri: Uri) {
+		context?.let {
+			CropImage.activity(cropImageUri)
+				.setActivityTitle("Cropping Smallest Cell")
+				.setGuidelines(CropImageView.Guidelines.ON)
+				.start(it, this)
+		}
+	}
+
+	override fun cropMaxRadius(cropImageUri: Uri) {
+		context?.let {
+			CropImage.activity(cropImageUri)
+				.setActivityTitle("Cropping Largest Cell")
+				.setGuidelines(CropImageView.Guidelines.ON)
+				.start(it, this)
+		}
+	}
+
 	override fun setUpView() {
 		chooseImageButton.setOnClickListener {
-			context?.let {
-				CropImage.activity()
-					.setGuidelines(CropImageView.Guidelines.ON)
-					.start(it, this)
-			}
+			val brightness = brightnessEditText.text.toString().toDouble()
+			val contrast = contrastEditText.text.toString().toDouble()
+			val minDistant = minDistantEditText.text.toString().toDouble()
+			mPresenter.setValue(brightness, contrast, minDistant)
+			cropImage()
 		}
 	}
 
@@ -69,17 +102,18 @@ class CellFragment : BaseMvpFragment<CellContract.View, CellContract.Presenter>(
 		if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
 			val result = CropImage.getActivityResult(data)
 			if (resultCode == Activity.RESULT_OK) {
-				val brightness = brightnessEditText.text.toString().toDouble()
-				val contrast = contrastEditText.text.toString().toDouble()
-				val minDistant = minDistantEditText.text.toString().toDouble()
-				val maxRadius = maxRadiusEditText.text.toString().toInt()
-				val minRadius = minRadiusEditText.text.toString().toInt()
-				mPresenter.findCellFromPlate(
-					result.uri,
-					brightness, contrast, minDistant, minRadius, maxRadius
-				)?.let {
-					imageView.setImageBitmap(it)
-				}
+				mPresenter.handleLogic(result.uri)
+//				val brightness = brightnessEditText.text.toString().toDouble()
+//				val contrast = contrastEditText.text.toString().toDouble()
+//				val minDistant = minDistantEditText.text.toString().toDouble()
+//				val maxRadius = maxRadiusEditText.text.toString().toInt()
+//				val minRadius = minRadiusEditText.text.toString().toInt()
+//				mPresenter.findCellFromPlate(
+//					result.uri,
+//					brightness, contrast, minDistant, minRadius, maxRadius
+//				)?.let {
+//					imageView.setImageBitmap(it)
+//				}
 			} else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
 				val error = result.error
 				Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
